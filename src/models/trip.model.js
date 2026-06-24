f2bf5b32d8ec75db4f595291faaaaa7ea616b60e
Chetan <chetanmittal39@gmail.com>
2026-06-25 02:34:12 +0530

    Trips Schema Created

---
+++ src/models/trip.model.js
@@ -0,0 +1,69 @@
+import mongoose from "mongoose";
+
+const tripSchema = new mongoose.Schema(
+    {
+        title: {
+            type: String,
+            required: true
+        },
+
+        description: {
+            type: String
+        },
+
+        destination: {
+            type: String,
+            required: true
+        },
+
+        startDate: {
+            type: Date,
+            required: true
+        },
+
+        endDate: {
+            type: Date,
+            required: true
+        },
+
+        owner: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+            required: true
+        },
+
+        members: [
+            {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: "User",
+            }
+        ],
+
+        coverPhoto: {
+            type: String,
+            default: ""
+        },
+
+        budgetLimit:{
+            type: Number,
+            default: 0
+        },
+
+        status: {
+            type: String,
+            enum: ["Not Started" , "In Progress" , "Completed"],
+            default: "Not Started"
+        },
+
+        shareLink: {
+            type: String,
+            unique: true,
+            sparse: true,                     // enforces unique on documents where this field has some value , ignore null values
+            default: null
+        }
+
+    },{timestamps: true}
+);
+
+
+export const Trip = mongoose.model("Trip" , tripSchema);
