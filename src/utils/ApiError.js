99edfcc2a302cda581e1496e92ab540f11c055e4
Chetan <chetanmittal39@gmail.com>
2026-06-26 02:26:24 +0530

    Implemented Wrappers for handling Errors and Responses

---
+++ src/utils/ApiError.js
@@ -0,0 +1,27 @@
+class ApiError extends Error {
+
+    constructor(
+        statusCode,
+        message = "Something Went Wrong",
+        stack = "",
+        errors = []
+    ) {
+
+        super(message)
+        this.statusCode = statusCode
+        this.message = message
+        this.errors = errors
+        this.data = null
+
+
+        if(stack){
+            this.stack = stack
+        }
+        else{
+            Error.captureStackTrace(this , this.constructor);
+        }
+    }
+}
+
+
+export {ApiError}
