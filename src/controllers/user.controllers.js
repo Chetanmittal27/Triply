27c38a18aec3d003a051504279b1e2d18ee6e1da
Chetan <chetanmittal39@gmail.com>
2026-06-27 01:20:09 +0530

    Register Controller

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -1,15 +1,17 @@
-import {asyncHandler} from "../utils/asyncHandler.js"
-import {ApiError} from "../utils/ApiError.js"
-import {User} from "../models/user.models.js"
+import {asyncHandler} from "../utils/asyncHandler.js";
+import {ApiError} from "../utils/ApiError.js";
+import {User} from "../models/user.models.js";
+import {uploadOnCloudinary} from "../utils/cloudinary.js";
+import {ApiResponse} from "../utils/ApiResponse.js";
 
 const registerUser = asyncHandler(async(req , res) => {
 
     // 1) Access Data entered by user
-    const {username , email , ...rest} = req.body
+    const {username , email , fullName, password , mobileNumber , gender} = req.body
 
     // 2) validate data entered by user
     if(
-        [username, email, ...rest].some((field) => 
+        [username, email, fullName, password , mobileNumber , gender].some((field) => 
             field?.trim() === "")
         ){
             throw new ApiError(400 , "All Fields are Required");
@@ -23,12 +25,54 @@ const registerUser = asyncHandler(async(req , res) => {
     );
 
 
-    if(!existedUser){
-        throw new ApiError(400 , "User not found");
+    if(existedUser){
+        throw new ApiError(409, "User with this Email or Username already exists");
     };
 
     
     // 4) Check for files
+    const avatarLocalPath = Array.isArray(req.files?.avatar) && req.files.avatar.length > 0 ? req.files.avatar[0].path : undefined;
+    
+    if(!avatarLocalPath){
+        throw new ApiError(400 , "Avatar is required");
+    }
+
+    const coverImageLocalPath = Array.isArray(req.files?.coverImage) && req.files.coverImage.length > 0 ? req.files.coverImage[0].path : undefined;
+
+
+    // 5) Upload files on cloudinary
+    const avatar = await uploadOnCloudinary(avatarLocalPath);
+    const coverImage = await uploadOnCloudinary(coverImageLocalPath);
+
+    if(!avatar){
+        throw new ApiError(500, "Failed to upload avatar");
+    }
+
+    // Create db
+    const user = await User.create({
+        username,
+        email,
+        fullName,
+        password,
+        mobileNumber,
+        gender,
+        avatar: avatar.url,
+        coverImage: coverImage?.url || ""
+    });
+
+    if(!user){
+        throw new ApiError(500 , "User Not Created");
+    }
+    
+    const createdUser = await User.findById(user._id).select(
+        "-password -refreshToken"
+    );
+
+
+    return res.status(201)
+    .json(
+        new ApiResponse(201 , createdUser , "User Registered Successfully")
+    );
 });
 
 
--- src/models/user.models.js
+++ src/models/user.models.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import bcrypt from "bcrypt"
 
 const userSchema = new mongoose.Schema(
     {
@@ -37,7 +38,8 @@ const userSchema = new mongoose.Schema(
         },
 
         coverImage: {
-            type: String
+            type: String,
+            default: ""
         },
 
         isVerified: {
@@ -64,5 +66,13 @@ const userSchema = new mongoose.Schema(
 
 );
 
+
+userSchema.pre("save" , async function(next){
+    if(!this.isModified("password")) return next();
+
+    this.password = await bcrypt.hash(this.password , 10);
+
+    next();
+});
 
 export const User = mongoose.model("User" , userSchema);
