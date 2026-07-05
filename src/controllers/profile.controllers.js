7cdbdcbfd744b6cd5d24497c04af76f7a55e72db
Chetanmittal27 <153277496+Chetanmittal27@users.noreply.github.com>
2026-07-05 21:00:34 +0000

    Update Profile Details Controller Created

--- src/controllers/profile.controllers.js
+++ src/controllers/profile.controllers.js
@@ -1,5 +1,5 @@
 import {asyncHandler} from "../utils/asyncHandler.js";
-import User from "../models/user.models.js";
+import {User} from "../models/user.models.js";
 import {ApiError} from "../utils/ApiError.js";
 import {ApiResponse} from "../utils/ApiResponse.js";
 
@@ -21,4 +21,44 @@ const getProfile = asyncHandler(async(req , res) => {
 });
 
 
-export {getProfile};
+const updateProfile = asyncHandler(async(req , res) => {
+
+    const {fullName , mobileNumber} = req.body;
+
+    if(!fullName && !mobileNumber){
+        throw new ApiError(400 , "At least one field fullName or mobileNumber is required");
+    }
+
+
+    const updateFields = {};
+
+    if(fullName) updateFields.fullName = fullName.trim();
+    if(mobileNumber) updateFields.mobileNumber = mobileNumber.trim();
+
+
+    const user = await User.findByIdAndUpdate(
+        req.user._id,
+        
+        {
+            $set: updateFields
+        },
+
+        {new: true}
+
+    ).select(
+        "-password -refreshToken -emailVerificationToken"
+    )
+
+    if(!user){
+        throw new ApiError(400 , "User not found");
+    }
+
+
+    return res.status(200)
+    .json(
+        new ApiResponse(200 , user , "Profile Updated Successfully")
+    )
+
+});
+
+export {getProfile , updateProfile};
