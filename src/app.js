08ec1b653956c4e5a28dec74d25d74a25e5000dd
Chetan <chetanmittal39@gmail.com>
2026-07-15 04:11:44 +0530

    app file

--- src/app.js
+++ src/app.js
@@ -24,6 +24,13 @@ app.use(
     credentials: true,
   })
 );
+
+app.options("*", cors({
+  origin: "https://triply-green.vercel.app",
+  credentials: true,
+}));
+
+
 app.use(express.json({ limit: "1mb" }));
 app.use(express.urlencoded({ extended: true, limit: "1mb" }));
 app.use(cookieParser());
