8c54d08971490c93de03ead92295ccd84b938da2
Chetan <chetanmittal39@gmail.com>
2026-07-15 13:24:53 +0530

    Removed cors options

--- src/app.js
+++ src/app.js
@@ -20,15 +20,15 @@ app.set("trust proxy", 1);
 
 app.use(
   cors({
-    origin: "https://triply-green.vercel.app",
+    origin: process.env.CORS_ORIGIN || "http://localhost:5173",
     credentials: true,
   })
 );
 
-app.options("*", cors({
-  origin: process.env.CORS_ORIGIN || "http://localhost:5173",
-  credentials: true,
-}));
+// app.options("*", cors({
+//   origin: process.env.CORS_ORIGIN || "http://localhost:5173",
+//   credentials: true,
+// }));
 
 
 app.use(express.json({ limit: "1mb" }));
